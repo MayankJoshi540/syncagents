2bd69b90bdddaf23cfcb5d5c69117873e7ffba41
Mayank Joshi <joshimayank646@gmail.com>
2026-07-10 14:32:05 +0530

    feat: initialize backend project with Redis service configuration and ioredis client

--- backend/shared/redis/redis.js
+++ backend/shared/redis/redis.js
@@ -1,23 +1,15 @@
-import Redis from 'ioredis'
-import dotenv from 'dotenv'
+import Redis from "ioredis";
+import dotenv from "dotenv";
+
 dotenv.config();
 
-const redisClient= new Redis(process.env.REDIS_URL);
+const redis = new Redis(process.env.REDIS_URL);
 
-redisClient.on("error", (err) => {
-    console.log("Redis connection error", err);
-});
-redisClient.on("connect", () => {
-    console.log("Redis connected");
-}); 
-
-redisClient.on("end", () => {
-    console.log("Redis connection closed");
+redis.on("connect", () => {
+  console.log("✅ Redis Connected");
 });
 
-process.on("SIGINT", () => {
-    redisClient.quit();
-    process.exit(0);
-});
 
-export default redisClient; 
+
+
+export default redis;
